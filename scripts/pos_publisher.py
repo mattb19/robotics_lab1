c7cc39c28add1df9f9ba66a9af8651c4cc82cfe5
mattb19 <mhb9863@uncw.edu>
2023-02-06 19:38:38 +0000

    finished lab with comments

--- scripts/pos_publisher.py
+++ scripts/pos_publisher.py
@@ -1,38 +1,54 @@
 #!/usr/bin/env python3
 
 import rospy
-# we are going to read turtlesim/Pose messages this time
+# read turtlesim/Pose messages
 from turtlesim.msg import Pose
-#importing the new message from our package
+# read the new message from our package
 from robotics_lab1.msg import Turtlecontrol
-# for radians to degrees conversions
-import math
+# read twist messages
+from geometry_msgs.msg import Twist
 
-ROTATION_SCALE = 180.0/math.pi
-
-pos_msg = Turtlecontrol()
+#calls Pose and Turtlecontrol.msg
+control_msg = Turtlecontrol()
+pos_msg = Pose()
 
 def pose_callback(data):
 	global pos_msg
-	# kp and xd
-	pos_msg.kp = data.kp
-	pos_msg.xd = data.xd
+	
+	# gets current position of node
+	pos_msg.x = data.x
+	
+
+
+def control_callback(data):
+	global control_msg
+	
+	# gets xd and kp from Turtlecontrol.msg
+	control_msg.xd = data.xd
+	control_msg.kp = data.kp
 	
 	
 if __name__ == '__main__':
 	# initialize the node
 	rospy.init_node('pos_publisher', anonymous = True)
-	# add a subscriber to it to read the position information
-	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, pose_callback)
-	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, pose_callback)
-	# add a publisher with a new topic using the Shortpos message
+	# add subscribers to it to read the position information
+	rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
+	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, control_callback)
+	# adding a publisher with a new topic
 	cmd_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
 	# set a 10Hz frequency for this loop
 	loop_rate = rospy.Rate(10)
+	
+	# initialize Twist
+	twist = Twist()
 
 	while not rospy.is_shutdown():
-		# publish the message
-		#pos_pub.publish(pos_msg)
+		# sets linear velocity
+		twist.linear.x = control_msg.kp*(control_msg.xd - pos_msg.x)
+		
+		# publish the message using twist
+		cmd_pub.publish(twist)
+		
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
 	
